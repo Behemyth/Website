7e995074eaa563bd9392611647977c2c880bbfe2
Asher Norland <asher.norland@gmail.com>
2022-08-05 07:23:59 -0400

    Dynamic Icon Sizing

--- components/ColorModeSwitch.vue
+++ components/ColorModeSwitch.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-const colorMode = useColorMode()
-const onClick = () => (colorMode.value === 'light' ? (colorMode.preference = 'dark') : (colorMode.preference = 'light'))
+const colorMode = useColorMode();
+const onClick = () =>
+    colorMode.value === "light"
+        ? (colorMode.preference = "dark")
+        : (colorMode.preference = "light");
 </script>
 
 <template lang="pug">
-button.inline-block.w-5(aria-label="Color Mode" @click="onClick")
-  ColorScheme(placeholder="🗘")
-    AppIcon(v-if="colorMode.value === 'dark'" name="heroicons-outline:moon" :size="10")
-    AppIcon(v-else name="heroicons-outline:sun" :size="10")
+button.inline-block.w-5(aria-label="Color Mode", @click="onClick")
+    ColorScheme(placeholder="🗘")
+        AppIcon(
+            v-if="colorMode.value === 'dark'",
+            name="heroicons-outline:moon",
+            :size="10"
+        )
+        AppIcon(v-else, name="heroicons-outline:sun", :size="5")
 </template>
