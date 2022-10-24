c0be486ad898393256d4c991efaf5c86753cfb64
Asher Norland <asher.norland@gmail.com>
2022-10-24 13:48:35 -0400

    Update Chore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
-const colors = require('tailwindcss/colors')
+import colors from 'tailwindcss/colors'
 
 module.exports = {
   darkMode: 'class',
   plugins: [
     require('@tailwindcss/typography')
   ],
-  content: ['content/**/*.{md,yml,json,json5,csv}'],
   theme: {
     extend: {
       colors: {
-        // Customize the feeling of your site
-        gray: colors.stone
+        gray: colors.gray
       }
     }
   }
