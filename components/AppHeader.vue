5f807ce8a7c9c1db930e9934868264c91994cb9d
Asher Norland <asher.norland@gmail.com>
2022-12-25 20:09:33 -0600

    Fix Lint Errors

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,11 +1,9 @@
 <template lang="pug">
 div.flex.justify-between.max-w-4xl.px-4.py-4.mx-auto(class="sm:px-8")
-  <!-- Navigation -->
   div.text-gray-700(class="dark:text-gray-200")
     ContentNavigation(v-slot="{ navigation }")
       NuxtLink.mr-4(v-for="link of navigation" :key="link._path" :to="link._path" active-class="font-bold") {{ link.navTitle || link.title }}
 
-  <!-- Social icons & Color Mode -->
   div.space-x-3.text-gray-500.transition
     ColorModeSwitch(class="hover:text-gray-700 dark:hover:text-gray-300")
 
