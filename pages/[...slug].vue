b57c0b969d385648a51e6016870f979836030da1
Asher Norland <asher.norland@gmail.com>
2022-12-28 17:18:39 -0600

    Initial Pages

---
+++ pages/[...slug].vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+div.max-w-4xl.px-4.py-10.mx-auto.bg-white(class="sm:px-8 sm:shadow dark:bg-gray-800 sm:rounded")
+    h1 There is no page called {{ $route.path }}
+</template>
+
+<style lang="postcss" scoped>
+/* Customize headers to remove default underline */
+.prose h2 a,
+.prose h3 a {
+    @apply no-underline;
+
+    &:hover {
+        @apply border-b border-neutral-600 border-neutral-300;
+    }
+}
+</style>
---
+++ pages/photography/[album]/[photo].vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+div.max-w-4xl.px-4.py-10.mx-auto.bg-white(class="sm:px-8 sm:shadow dark:bg-gray-800 sm:rounded")
+	ContentDoc.prose.prose-gray.max-w-none(class="dark:prose-invert")
+		template(#not-found)
+			h1 Photo not found
+		template(#empty)
+			h1 Loading photo...
+</template>
+
+<style lang="postcss" scoped>
+/* Customize headers to remove default underline */
+.prose h2 a,
+.prose h3 a {
+	@apply no-underline;
+
+	&:hover {
+		@apply border-b border-neutral-600 border-neutral-300;
+	}
+}
+</style>
