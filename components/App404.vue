93fbc4172728d8d3ecc5fe3618ce4f568f06e8ec
Asher Norland <asher.norland@gmail.com>
2022-12-29 12:28:26 -0600

    App404 Component

---
+++ components/App404.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+h1 What is this {{ name }} is not there
+
+</template>
+
+<script setup lang="ts">
+defineProps({
+	name: {
+		type: String,
+		required: true
+	}
+})
+
+</script>
