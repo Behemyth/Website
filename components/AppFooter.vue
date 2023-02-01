4261f20285d67ebe61b6bcd035935b74ba3a9d43
Asher Norland <asher.norland@gmail.com>
2023-02-01 18:14:17 -0500

    Replace Footer Name Link

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,7 +2,7 @@
 footer.p-4.bg-white.shadow.max-w-4xl.mx-auto(class="dark:bg-gray-900")
 	div(class="md:flex sm:items-center md:justify-between")
 		.mb-6(class="md:mb-0")
-			NuxtLink.flex.items-center(to="/")
+			NuxtLink.flex.items-center(to="contact")
 				img.mr-3.h-12.rounded-full(
 					src="https://www.gravatar.com/avatar/293a56bef971ab4999d6230491957d33?s=200",
 					alt="Asher's Face"
@@ -10,11 +10,6 @@ footer.p-4.bg-white.shadow.max-w-4xl.mx-auto(class="dark:bg-gray-900")
 				span.self-center.text-2xl.font-semibold.whitespace-nowrap(
 					class="dark:text-white"
 				) Asher Norland
-		.flex.space-x-6
-			NuxtLink(to="contact")
-				span.self-center.text-l.whitespace-nowrap(
-					class="dark:text-white"
-				) Contact
 
 		.flex.space-x-6
 			NuxtLink(v-for="social in data", :key="social.name", :to="social.link")
