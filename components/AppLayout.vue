952a6d850dfff7012b2cd2a46aef70d7a416bf2b
Asher Norland <asher.norland@gmail.com>
2023-02-01 15:40:22 -0500

    AppLayout Component (#76)

---
+++ components/AppLayout.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+div
+  Html(lang="en")
+  AppHeader
+  slot
+  AppFooter
+</template>
+
+<style lang="postcss">
+body {
+  @apply bg-primary-50 dark:bg-primary-900;
+}
+</style>
