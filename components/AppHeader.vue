668f97e3d98c4a67e5c8a8556a5f94f400ab066b
Asher Norland <asher.norland@gmail.com>
2023-02-07 10:52:36 -0500

    Search Compoennt

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-nav.bg-white.border-gray-200.px-4.py-4.rounded.max-w-4xl.mx-auto(
-	class="sm:px-4 dark:bg-gray-900"
+nav.bg-white.border-gray-200.p-4.rounded.max-w-4xl.mx-auto(
+	class="sm:px-2 dark:bg-gray-900"
 )
 	.container.flex.flex-wrap.items-center.justify-between.mx-auto
 		NuxtLink.flex.items-center(to="/")
@@ -13,35 +13,8 @@ nav.bg-white.border-gray-200.px-4.py-4.rounded.max-w-4xl.mx-auto(
 				class="dark:text-white"
 			) Home
 		.flex(class="md:order-2")
-			button.text-gray-500.rounded-lg.text-sm.p-2.mr-1(
-				type="button",
-				data-collapse-toggle="navbar-search",
-				aria-controls="navbar-search",
-				aria-expanded="false",
-				class="md:hidden dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700"
-			)
-				Icon.w-5.h-5(
-					name="fa6-solid:magnifying-glass"
-					size="24"
-					aria-hidden="true"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-				)
-				span.sr-only Search
-			.relative.hidden(class="md:block")
-				.absolute.inset-y-0.left-0.flex.items-center.pl-3.pointer-events-none
-					Icon.text-gray-500.w-5.h-5(
-						name="fa6-solid:magnifying-glass"
-						size="24"
-					)
-					span.sr-only Search icon
-				input.block.w-full.p-2.pl-10.text-sm.text-gray-900.border.border-gray-300.rounded-lg.bg-gray-50(
-					id="search-navbar"
-					type="text"
-					class="focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-					placeholder="Search..."
-				)
-			button.inline-flex.items-center.p-2.text-sm.text-gray-500.rounded-lg(
+			Search
+			button.inline-flex.items-center.p-4.text-sm.text-gray-500.rounded-lg(
 				data-collapse-toggle="navbar-search"
 				type="button"
 				class="md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -51,10 +24,9 @@ nav.bg-white.border-gray-200.px-4.py-4.rounded.max-w-4xl.mx-auto(
 				span.sr-only Open menu
 				Icon.w-5.h-5(
 					name="material-symbols:menu"
-					size="24"
 					class="text-gray-500"
 				)
-			.space-x-3.text-gray-500.transition.p-2
+			.space-x-3.text-gray-500.transition.p-4
 				ColorModeSwitch(
 					class="hover:text-gray-700 dark:hover:text-gray-300"
 				)
@@ -62,30 +34,18 @@ nav.bg-white.border-gray-200.px-4.py-4.rounded.max-w-4xl.mx-auto(
 			id="navbar-search"
 			class="md:flex md:w-auto md:order-1"
 		)
-			.relative.mt-3(
+			Search(
 				class="md:hidden"
 			)
-				.absolute.inset-y-0.left-0.flex.items-center.pl-3.pointer-events-none
-					Icon.w-5.h-5(
-						name="fa6-solid:magnifying-glass"
-						size="24"
-						class="text-gray-500"
-					)
-				input.block.w-full.p-2.pl-10.text-sm.text-gray-900.border.border-gray-300.rounded-lg.bg-gray-50(
-					id="search-navbar"
-					type="text"
-					class="focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-					placeholder="Search..."
-				)
-			.flex.flex-col.p-4.mt-4.border.border-gray-100.rounded-lg.bg-gray-50(
-				class="md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+			.flex.flex-col.p-4.border.border-gray-100.rounded-lg.bg-gray-50(
+				class="md:flex-row md:space-x-4 md:items-center md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
 			)
-				NuxtLink.block.py-2.pl-3.pr-4.text-white.bg-blue-700.rounded(
+				NuxtLink.block.py-4.pl-2.pr-2.text-gray-700.rounded(
 					v-for="link of navigation"
 					:key="link._path"
 					:to="link._path"
 					active-class="font-bold"
-					class="md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+					class="md:p-0 dark:text-gray-400"
 				) {{ link.navTitle || link.title }}
 </template>
 
---
+++ components/Search.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+.flex
+	label(
+		for="search-dropdown"
+		class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+	)
+	button(
+		id="dropdown-button"
+		data-dropdown-toggle="dropdown"
+		class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+		type="button"
+	) All categories
+		Icon.w-5.h-5(
+			name="fa6-solid:magnifying-glass"
+			class="text-gray-500"
+		)
+
+	div(id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700")
+		ul(class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button")
+		li
+			button(
+				type="button"
+				class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+			) Test Category
+	.relative.w-full
+		input(
+			id="search-dropdown"
+			type="search"
+			class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+			placeholder="Search..."
+			required
+		)
+		button(
+			type="submit"
+			class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+		)
+			Icon.w-5.h-5(
+				name="fa6-solid:magnifying-glass"
+				class="text-gray-500"
+			)
+			span.sr-only Search
+</template>
+
+<script setup lang="ts">
+</script>
