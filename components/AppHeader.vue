55dc35c32b0b5dde338bb06b12201164400b5b1c
Asher Norland <asher.norland@gmail.com>
2023-02-09 18:07:18 -0500

    Compomnenmt

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -13,7 +13,7 @@ nav.bg-white.border-gray-200.p-4.rounded.max-w-4xl.mx-auto(
 				class="dark:text-white"
 			) Home
 		.flex(class="md:order-2")
-			Search
+			NavSearch(:list="search? search: []")
 			button.inline-flex.items-center.p-4.text-sm.text-gray-500.rounded-lg(
 				data-collapse-toggle="navbar-search"
 				type="button"
@@ -30,25 +30,12 @@ nav.bg-white.border-gray-200.p-4.rounded.max-w-4xl.mx-auto(
 				ColorModeSwitch(
 					class="hover:text-gray-700 dark:hover:text-gray-300"
 				)
-		.items-center.justify-between.hidden.w-full(
-			id="navbar-search"
-			class="md:flex md:w-auto md:order-1"
-		)
-			Search(
-				class="md:hidden"
-			)
-			.flex.flex-col.p-4.border.border-gray-100.rounded-lg.bg-gray-50(
-				class="md:flex-row md:space-x-4 md:items-center md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
-			)
-				NuxtLink.block.py-4.pl-2.pr-2.text-gray-700.rounded(
-					v-for="link of navigation"
-					:key="link._path"
-					:to="link._path"
-					active-class="font-bold"
-					class="md:p-0 dark:text-gray-400"
-				) {{ link.navTitle || link.title }}
+		NavList(:list="navigation? navigation: []")
+		NavSearch(:list="search? search: []")
 </template>
 
 <script setup lang="ts">
 const { data: navigation } = useLazyAsyncData('navigation', () => fetchContentNavigation().then(navigation => navigation.slice(1)))
+const { data: search } = useLazyAsyncData('search', () => fetchContentNavigation().then(navigation => navigation.slice(1, -1)))
+
 </script>
