da835f143a7aa48f27abde840072c9458f5e9e8a
Asher Norland <asher.norland@gmail.com>
2023-02-19 13:12:57 -0500

    Icon Padding

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -3,18 +3,21 @@ nav.max-w-5xl.w-full.mx-auto.p-4.bg-white.shadow(
 	class="dark:bg-gray-900"
 )
 	.flex.items-center.justify-between.mx-auto
-		NuxtLink.flex.items-center(to="/")
-			img.h-6.mr-3.rounded-full(
+		NuxtLink.flex.flex-none.items-center.p-2(
+			to="/",
+			class="sm:p-4",
+		)
+			img.h-6.mr-1.rounded-full(
 				src="https://www.gravatar.com/avatar/293a56bef971ab4999d6230491957d33?s=200",
-				class="sm:h-9",
+				class="sm:h-9 sm:mr-3",
 				alt="Asher's Face"
 			)
-			span.self-center.text-xl.font-semibold.whitespace-nowrap(
-				class="dark:text-white"
+			span.self-center.text-l.font-semibold.whitespace-nowrap(
+				class="sm:text-xl dark:text-white"
 			) Home
 		NavList(:list="navigation? navigation: []")
 		NavSearch(:list="search? search: []")
-		ColorModeSwitch(
+		ColorModeSwitch.flex-none(
 			class="hover:text-gray-700 dark:hover:text-gray-300"
 		)
 </template>
