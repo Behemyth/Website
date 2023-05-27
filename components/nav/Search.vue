e9a699abf67af27f222739a285c7754c29944b2f
Asher Norland <asher.norland@gmail.com>
2023-05-27 12:14:47 -0400

    Update Search.vue

--- components/nav/Search.vue
+++ components/nav/Search.vue
@@ -4,19 +4,9 @@
 		input(
 			id="search-dropdown"
 			type="search"
-			class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-gray-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+			class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
 			:placeholder="props.navigation.name"
-			required
 		)
-		button(
-			type="submit"
-			class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-gray-700 rounded-r-lg border border-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-		)
-			Icon.w-5.h-5(
-				name="fa6-solid:magnifying-glass"
-				class="text-gray-500"
-			)
-			span.sr-only Search
 </template>
 
 <script setup lang="ts">
