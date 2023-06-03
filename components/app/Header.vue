90904ccab5668b55796696077c937189c1a755aa
Asher Norland <asher.norland@gmail.com>
2023-06-02 20:46:27 -0400

    Fix Up Logo

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -4,17 +4,14 @@ nav.max-w-5xl.w-full.mx-auto.p-4.bg-white.shadow(
 )
 	.flex.items-center.justify-between.mx-auto
 		NuxtLink.flex.flex-none.items-center.p-2(
-			to="/",
+			to="/contact",
 			class="md:p-4",
 		)
-			img.h-6.mr-1.rounded-full(
+			img.h-9.mr-1.rounded-full(
 				src="https://www.gravatar.com/avatar/293a56bef971ab4999d6230491957d33?s=200",
-				class="md:h-9 md:mr-3",
+				class="md:h-12",
 				alt="Asher's Face"
 			)
-			span.self-center.text-l.font-semibold.whitespace-nowrap(
-				class="md:text-xl dark:text-white"
-			) Home
 		NavList.hidden(
 			:list="navigation? navigation: []"
 			class="md:flex"
