792a96599621b95cdd7c36bd60ab07142a7b9e67
Asher Norland <asher.norland@gmail.com>
2023-07-20 17:35:41 -0400

    Update List.vue

--- components/nav/List.vue
+++ components/nav/List.vue
@@ -6,8 +6,7 @@
 		v-for="link of props.list"
 		:key="link._path"
 		:to="link._path"
-		active-class="font-bold"
-		class="dark:text-gray-400"
+		class="dark:text-gray-400 focus:font-bold"
 	) {{ link.navTitle || link.title }}
 </template>
 
