9e49ea544e59587540afad4c8c661e459ded0db3
Asher Norland <asher.norland@gmail.com>
2024-02-15 20:34:50 -0500

    Replace Script With Mount (#300)

--- layouts/review.vue
+++ layouts/review.vue
@@ -2,6 +2,21 @@
 div
 	main.prose.max-w-5xl.px-4.py-4.mx-auto(class="dark:prose-invert")
 		slot
-	script(defer src="https://cdn.commento.io/js/commento.js")
+
 	div(id="commento")
 </template>
+
+<script setup lang="ts">
+
+onMounted(() => {
+	if (!document.getElementById('commento_script')) {
+		const commentoScript = document.createElement('script')
+		commentoScript.setAttribute('src', 'https://cdn.commento.io/js/commento.js')
+		commentoScript.defer = true
+		commentoScript.id = 'commento_script'
+
+		document.head.appendChild(commentoScript)
+	}
+})
+
+</script>
