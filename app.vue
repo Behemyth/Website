ea2bf2f35d6dabb74fee8738601a75aa17bef6d4
Asher Norland <asher.norland@gmail.com>
2024-03-19 11:28:41 -0400

    RSS Headers

--- app.vue
+++ app.vue
@@ -3,14 +3,21 @@ NuxtPage
 </template>
 
 <script setup lang="ts">
+import type { ParsedContent } from '@nuxt/content/dist/runtime/types'
+
+const links =
+	await queryContent('/').where({ layout: 'feed' }).find().then((value) => {
+		return value.map((content: ParsedContent) => {
+			return {
+				rel: 'alternate',
+				type: 'application/rss+xml',
+				title: content.title,
+				href: 'https://www.ashernorland.com/rss/' + content._path
+			}
+		})
+	})
+
 useHead({
-	link: [
-		{
-			rel: 'alternate',
-			type: 'application/rss+xml',
-			title: 'RSS Feed',
-			href: 'https://www.ashernorland.com/rss'
-		}
-	]
+	link: links
 })
 </script>
