ae891c90ef59fcbea2fb3c9c654fbfb0fcdb61b7
Asher Norland <asher.norland@gmail.com>
2024-04-27 16:34:30 -0400

    Update ReviewPreview.vue

--- components/content/ReviewPreview.vue
+++ components/content/ReviewPreview.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 ULink.h-32.flex.w-full.items-center.bg-white.border.border-gray-200.rounded-lg.shadow(:to="props.content._path", :title="data?.title" class="md:h-48 lg:h-64 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700")
-	NuxtPicture(:src="imgURL" preload class="aspect-[2/3] md:h-48 lg:h-64" :img-attrs="{class:'rounded-s-lg'}")
+	NuxtPicture.h-32(:src="imgURL" preload class="aspect-[2/3] md:h-48 lg:h-64" :img-attrs="{class:'rounded-s-lg'}")
 	.flex.flex-col.justify-between.p-4.leading-normal
 		h5.mb-2.font-bold.tracking-tight.text-gray-900(class="md:text-l lg:text-xl dark:text-white") {{ data?.title }}
 		Rating(:value="content.rating" :size="16")
