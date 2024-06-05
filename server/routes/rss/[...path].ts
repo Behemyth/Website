18e2454a913142b69705dd47b4fd92b7c4e02beb
Asher Norland <asher.norland@gmail.com>
2024-06-05 12:45:33 -0400

    yaaaaaaaa

--- server/routes/rss/[...path].ts
+++ server/routes/rss/[...path].ts
@@ -1,6 +1,6 @@
 import { toXML } from 'jstoxml'
 import { serverQueryContent } from '#content/server'
-import type { NuxtContent, NuxtFeed, RSSFeed, RSSEntry, RSSChannel } from '~/types/content'
+import type { NuxtContent, NuxtFeed, RSSFeed, RSSEntry, RSSChannel, RSSItem } from '~/types/content'
 
 export default defineEventHandler(async (event) => {
 	const path = getRouterParam(event, 'path')
@@ -11,22 +11,22 @@ export default defineEventHandler(async (event) => {
 		title: feedContent.title,
 
 		description: feedContent.description,
-		id: feedContent._id,
+		// id: feedContent._id,
 		link: 'https://ashernorland.com/' + path,
 		language: 'en',
 		image: feedContent.image,
-		favicon: 'http://ashernorland.com/favicon.ico',
+		// favicon: 'http://ashernorland.com/favicon.ico',
 		copyright: 'Copyright © ' + new Date().getFullYear() + ' Asher Norland',
-		updated: new Date(),
+		// updated: new Date(),
 		generator: 'Feed for Node.js',
-		feedLinks: {
-			rss: 'https://ashernorland.com/rss/' + path
-		},
-		author: {
-			name: 'Asher Norland',
-			email: 'asher.norland@gmail.com',
-			link: 'https://ashernorland.com/contact'
-		},
+		// feedLinks: {
+		// 	rss: 'https://ashernorland.com/rss/' + path
+		// },
+		// author: {
+		// 	name: 'Asher Norland',
+		// 	email: 'asher.norland@gmail.com',
+		// 	link: 'https://ashernorland.com/contact'
+		// },
 		item: []
 	}
 
@@ -41,22 +41,23 @@ export default defineEventHandler(async (event) => {
 	const docs = await serverQueryContent<NuxtContent>(event).sort({ date: -1 }).where({ _partial: false }).find()
 
 	for (const post of docs) {
-		feed.rss.channel.item.push({
+		const item: RSSItem = {
 			title: post.title,
-			id: post._id,
-			link: post._path,
-			description: post.description,
-			content: post.content,
-			author: [
-				{
-					name: 'Asher Norland',
-					email: 'asher.norland@gmail.com',
-					link: 'https://ashernorland.com/contact'
-				}
-			],
-			date: post.date,
-			image: post.image
-		})
+			// id: post._id,
+			// link: post._path,
+			description: post.description
+			// content: post.content,
+			// author: [
+			// 	{
+			// 		name: 'Asher Norland',
+			// 		email: 'asher.norland@gmail.com',
+			// 		link: 'https://ashernorland.com/contact'
+			// 	}
+			// ],
+			// date: post.date,
+			// image: post.image
+		}
+		feed.rss.channel.item.push(item)
 	}
 
 	const config = {
