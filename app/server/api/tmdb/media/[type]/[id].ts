5c587661dcf6e5968544f7b2f72dc08c23426fd6
Asher Norland <asher.norland@gmail.com>
2024-08-22 13:47:37 -0400

    Remove Intermediate Error Check

--- app/server/api/tmdb/media/[type]/[id].ts
+++ app/server/api/tmdb/media/[type]/[id].ts
@@ -6,20 +6,13 @@ export default defineEventHandler(async (event) => {
 	if (!config.apiSecret) { throw new Error('TMDB API key is missing') }
 	if (!config.public.apiBase) { throw new Error('TMDB API URL is missing') }
 
-	try {
-		return await $fetch(`/${type}/${id}`, {
-			baseURL: `${config.public.apiBase}`,
-			params: {
-				language: 'en-US'
-			},
-			headers: {
-				Authorization: `Bearer ${config.apiSecret}`
-			}
-		})
-	} catch (error: unknown) {
-		setResponseStatus(event, 500)
-		return {
-			error: "An error occurred while fetching the data",
+	return await $fetch(`/${type}/${id}`, {
+		baseURL: `${config.public.apiBase}`,
+		params: {
+			language: 'en-US'
+		},
+		headers: {
+			Authorization: `Bearer ${config.apiSecret}`
 		}
-	}
+	})
 })
