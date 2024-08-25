cab7e38144df301cb162218e0fd2453e6464d059
Asher Norland <asher.norland@gmail.com>
2024-08-25 00:00:39 -0400

    Conditional Giscus Theme

--- app/components/DiscussionList.vue
+++ app/components/DiscussionList.vue
@@ -1,5 +1,6 @@
 <template>
 	<Giscus
+	v-if="colorMode.preference === 'dark'"
 	repo="Behemyth/Website"
 	repo-id="R_kgDOGNxb4w"
 	:category="props.category"
@@ -13,10 +14,27 @@
 	lang="en"
 	loading="lazy"
 	/>
+	<Giscus
+	v-else
+	repo="Behemyth/Website"
+	repo-id="R_kgDOGNxb4w"
+	:category="props.category"
+	category-id="DIC_kwDOGNxb484Chz_u"
+	mapping="title"
+	strict="1"
+	reactions-enabled="1"
+	emit-metadata="1"
+	input-position="top"
+	theme="light"
+	lang="en"
+	loading="lazy"
+	/>
 </template>
 
 <script setup lang="ts">
 import Giscus from '@giscus/vue'
+
+const colorMode = useColorMode()
 
 const props = defineProps({
 	category: {
